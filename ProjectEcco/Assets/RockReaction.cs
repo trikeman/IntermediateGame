aa63b9f28e7cabef5456a79403e05d74d683e2df
TJ Darcy <darcyt@usc.edu>
2013-10-08 12:43:38 -0700

    Platforms react to rock monster.

---
+++ ProjectEcco/Assets/RockReaction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockReaction : MonoBehaviour {
+	
+	public GameObject monster;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(rigidbody.velocity!=Vector3.zero){
+			rigidbody.velocity=Vector3.Lerp (rigidbody.velocity,Vector3.zero,1f);
+		}
+	}
+	
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject==monster){
+			rigidbody.velocity=monster.rigidbody.velocity*100;
+		}
+	}
+}
