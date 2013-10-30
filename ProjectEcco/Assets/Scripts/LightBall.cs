5f12e0e49f5f4b1d5864ad7d162cb6c56082d246
Espeo12345 <zmziauddin@guest-wireless-eup-nat-206-117-102-004.usc.edu>
2013-10-30 16:26:14 -0700

    LightBall Mechanic Implemented

---
+++ ProjectEcco/Assets/Scripts/LightBall.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBall : MonoBehaviour {
+	
+	public bool lighted;
+	public string lightBeamTag = "LightBeam";
+	public string darkWallTag = "DarkWall";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == lightBeamTag){
+			Destroy(col.gameObject);
+			lighted = true;
+		}else if (col.gameObject.tag == darkWallTag){
+			if (lighted == true){
+			Destroy(col.gameObject);
+			Destroy(this.gameObject);
+			}
+		}
+	}
+}
