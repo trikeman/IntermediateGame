283201910b33ea09913b18bff62a7653890d2ac8
Espeo12345 <zmziauddin@guest-wireless-eup-nat-206-117-102-004.usc.edu>
2013-10-31 10:39:39 -0700

    First Four Sections Leveled Out
    Still need to be organized

--- ProjectEcco/Assets/Scripts/LightBall.cs
+++ ProjectEcco/Assets/Scripts/LightBall.cs
@@ -6,16 +6,27 @@ public class LightBall : MonoBehaviour {
 	public bool lighted;
 	public string lightBeamTag = "LightBeam";
 	public string darkWallTag = "DarkWall";
-
+	private Color darkColor = Color.black;
+	private Color lightColor = Color.white;
+	public float duration = 10f;
+	public Light thisSpotlight;
+	
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (lighted == false){
+			thisSpotlight.enabled = false;
+			renderer.material.color = darkColor;
+		}
+		else if (lighted == true){
+			thisSpotlight.enabled = true;
+			renderer.material.color = lightColor;
+		}
+		}
 	
-	}
 	
 	void OnCollisionEnter(Collision col){
 		if (col.gameObject.tag == lightBeamTag){
