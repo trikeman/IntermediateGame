f25e1fa823345d6a1dfcb04b71da9797a31313ce
Espeo12345 <zmziauddin@guest-wireless-eup-nat-206-117-102-004.usc.edu>
2013-11-02 03:29:18 -0700

    Menu Screen

---
+++ ProjectEcco/Assets/Cutscenes/Highlight.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Highlight : MonoBehaviour {
+
+private Color startcolor;
+void OnMouseEnter()
+{
+    startcolor = renderer.material.color;
+   renderer.material.color = Color.yellow;
+}
+void OnMouseExit()
+{
+    renderer.material.color = startcolor;
+}
+}
---
+++ ProjectEcco/Assets/Cutscenes/PlayGame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayGame : MonoBehaviour {
+	
+
+private Color startcolor;
+void OnMouseOver()
+{
+    startcolor = renderer.material.color;
+   renderer.material.color = Color.yellow;
+		
+		  if (Input.GetMouseButtonDown(0)){
+			//bookScene.animation.Play("Book Opening");
+			Debug.Log("let's load things");
+			Application.LoadLevel("UnderTheBed");
+		}
+             
+}
+void OnMouseExit()
+{
+    renderer.material.color = startcolor;
+}
+}
