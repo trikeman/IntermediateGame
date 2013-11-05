cba1e57b65a35860cec6c52e173ac8bae264a2e7
Espeo12345 <zmziauddin@guest-wireless-eup-nat-206-117-102-004.usc.edu>
2013-11-04 16:43:16 -0800

    Ending Animation and Fix to the Last Area

---
+++ ProjectEcco/Assets/Cutscenes/EndGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+		void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player"){
+			Application.LoadLevel("Ending Scene");
+			
+		} 
+	}
+}
