7c2aff65f8a5da24464ccc316b5a0d313180fccd
Espeo12345 <zmziauddin@guest-wireless-eup-nat-206-117-102-004.usc.edu>
2013-11-17 02:38:14 -0800

    Who is that Sexy Yet Poorly Recorded Narrator

---
+++ ProjectEcco/Assets/Scripts/VoiceTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoiceTrigger : MonoBehaviour {
+	
+	public AudioClip thisEvent;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player"){
+			audio.PlayOneShot(thisEvent, 0.8F);
+			Destroy(this.gameObject);
+		}
+	}
+}
