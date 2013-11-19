105e773ac33c0a4ffebd6c084fb9810d52b8f25c
TJ Darcy <darcyt@usc.edu>
2013-11-18 16:21:13 -0800

    Light Monster Now Shoots in Direction of Camera

--- ProjectEcco/Assets/Scripts/LightMonsterAttack.cs
+++ ProjectEcco/Assets/Scripts/LightMonsterAttack.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class LightMonsterAttack : MonoBehaviour {
 	
 	public GameObject beam;
+	public GameObject camera;
 	public int force = 500;
 	public Metrics metrics;
 	
@@ -17,8 +18,11 @@ public class LightMonsterAttack : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(follow.following&&Input.GetButtonDown("Fire2")){
-			GameObject newBeam = (GameObject)GameObject.Instantiate(beam, transform.position+follow.player.transform.forward.normalized,transform.rotation);
-			newBeam.rigidbody.AddForce(follow.player.transform.forward*force);
+			Vector3 direction = camera.transform.forward;
+			direction.y=0;
+			direction.Normalize();
+			GameObject newBeam = (GameObject)GameObject.Instantiate(beam, transform.position+direction,transform.rotation);
+			newBeam.rigidbody.AddForce(direction*force);
 			metrics.LightUses++;
 		}
 	}
