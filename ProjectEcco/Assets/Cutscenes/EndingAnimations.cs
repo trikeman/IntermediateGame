d61338759f38489750c863f4130ea96de126504a
Espeo12345 <zmziauddin@wireless-plus-006-049.usc.edu>
2013-12-02 22:40:23 -0800

    Ending Animation Implemented (Numbers Need Tweaking)

---
+++ ProjectEcco/Assets/Cutscenes/EndingAnimations.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndingAnimations : MonoBehaviour {
+	
+	public GameObject theBook;
+	public GameObject mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DoAnimation());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+			IEnumerator DoAnimation()
+{
+  yield return new WaitForSeconds(5);
+		theBook.animation.Play("Page 2");
+	yield return new WaitForSeconds(6);
+		theBook.animation.Play("Page 3");
+		yield return new WaitForSeconds(5);
+		theBook.animation.Play("Book Closing");
+		yield return new WaitForSeconds(1);
+		mainCamera.animation.Play("EndingPanOut");
+}
+}
